74cb80cd002368609d7c7c18036e84411e2e1557
max-im <pogidaevmo@gmail.com>
2025-04-19 03:59:14 +0300

    DEV: ADD: job details data

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -4,17 +4,24 @@ import { useCallback, useState } from "react";
 import { useFetch } from "../../hook/useFetch";
 
 import { COLORS, icons, images, SIZES } from "../../constants";
-import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } from "../../components";
+import { Company, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } from "../../components";
 
-const tabs = ["About", "Qualifications", "Responsibilities"];
 
+// http://localhost:8081/job-details/R2xJuBjzz6FB2aIZAAAAAA==
 export default function JobDetails() {
     const { id } = useLocalSearchParams();
     const router = useRouter();
-
+    
     const [refresh, setRefresh] = useState(false);
-    const {data, isLoading, error, refetch} = useFetch('job-details', { job_id: id });
-    const [activeTab, setActiveTab] = useState(tabs[0]);
+    const { data, isLoading, error, refetch } = useFetch('job-details', { job_id: id });
+
+    const tabs = {
+        About: <Specifics title='About' points={data[0]?.job_description.split('\n') ?? ['N/A']} />,
+        Qualifications: <Specifics title='Qualifications' points={data[0]?.job_highlights?.Qualifications ?? ['N/A']} />,
+        Responsibilities: <Specifics title='Responsibilities' points={data[0]?.job_highlights?.Responsibilities ?? ['N/A']} />,
+    };
+    
+    const [activeTab, setActiveTab] = useState(Object.keys(tabs)[0]);
 
     const onRefresh = () => { };
 
@@ -49,14 +56,16 @@ export default function JobDetails() {
                                 location={data[0].job_country}
                             />
 
-                            <JobTabs 
-                                tabs={tabs}
+                            <JobTabs
+                                tabs={Object.keys(tabs)}
                                 activeTab={activeTab}
                                 setActiveTab={setActiveTab}
                             />
+                            {tabs[activeTab]}
                         </View>
                     )}
                 </ScrollView>
+                <JobFooter url={data[0]?.job_google_link} />
             </>
         </SafeAreaView>
     )
--- components/jobdetails/tabs/Tabs.jsx
+++ components/jobdetails/tabs/Tabs.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, FlatList } from 'react-native'
 
-import styles from './tabs.style'
+import styles from './tabs.style';
+import { SIZES } from '../../../constants';
 
-const Tabs = () => {
+function TabButton({ name, activeTab, onClick }) {
+  return (
+    <TouchableOpacity
+      style={styles.btn(name, activeTab)}
+      onPress={onClick}
+    >
+      <Text style={styles.btnText(name, activeTab)}>{name}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const Tabs = ({ tabs, activeTab, setActiveTab }) => {
   return (
     <View>
-      <Text>Tabs</Text>
+      <FlatList
+        data={tabs}
+        renderItem={({ item }) => (<TabButton name={item} activeTab={activeTab} onClick={() => setActiveTab(item)} />)}
+        horizontal
+        keyExtractor={(item) => item}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ columnGap: SIZES.small / 2 }}
+      />
     </View>
   )
 }
